c30d1667a0136a5c6228a759ec435f61e15b6c6c
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-03-09 18:23:03 -0500

    Make characters customizable and change emoji to optional with -e

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,54 @@
 extern crate dirs;
 extern crate pancurses;
+extern crate clap;
 
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use pancurses::*;
+use clap::{Arg, App};
 
 fn main() {
+    let matches = App::new("Task List")
+                    .version("1.0.1")
+                    .author("ThePuzzlemaker <tpzker@thepuzzleamker.info>")
+                    .about("A simple program to track tasks to do regularly. It does not save completed tasks.")
+                    .arg(Arg::with_name("custom_chars")
+                            .short("c")
+                            .long("custom_chars")
+                            .value_name("chars")
+                            .help("Sets the characters for use with the task list. (Example: -c \"X√\" will use 'X' for an uncompleted task and '√' for a completed task)")
+                            .takes_value(true))
+                    .arg(Arg::with_name("emoji")
+                            .short("e")
+                            .long("emoji")
+                            .takes_value(false)
+                            .help("Uses emoji for the task list if this flag is present."))
+                    .get_matches();
+
+    let emoji_chars: [char; 2] = ['❌', '✅'];
+    let standard_chars: [char; 2] = ['X', '√'];
+    let mut chars: Vec<char> = vec![];
+
+    if !matches.is_present("custom_chars") {
+        if matches.is_present("emoji") {
+            chars.push(emoji_chars[0]);
+            chars.push(emoji_chars[1]);
+        } else {
+            chars.push(standard_chars[0]);
+            chars.push(standard_chars[1]);
+        }
+    } else {
+        let value = matches.value_of("custom_chars").unwrap_or("X√");
+        if value.len() != 2 {
+            panic!("Error: only two characters can be provided for custom characters.");
+        } else {
+            chars.push(value.chars().nth(0).unwrap_or('X'));
+            chars.push(value.chars().nth(1).unwrap_or('√'));
+        }
+    }
+
+
     let window = initscr();
     noecho();
     curs_set(0);
@@ -25,7 +67,7 @@ fn main() {
     
     window.attron(COLOR_PAIR(1));
     window.attron(A_BOLD);
-    window.addstr("Welcome to Task List v1.0.0. Press any key to mark a task as complete.\n");
+    window.addstr("Welcome to Task List v1.0.1. Press any key to mark a task as complete.\n");
     window.attroff(COLOR_PAIR(1));
     window.attroff(A_BOLD);
     window.refresh();
@@ -63,7 +105,7 @@ fn main() {
 
     for line in s.lines() {
         window.attron(COLOR_PAIR(4));
-        window.addstr("[❎] ");
+        window.addstr(&format!("[{}] ", chars[0]));
         window.attroff(COLOR_PAIR(4));
         window.addstr(&format!("{}\n", line.trim_end()));
     }
@@ -71,7 +113,7 @@ fn main() {
     for _i in 0..s.lines().count() {
         window.getch();
         window.attron(COLOR_PAIR(3));
-        window.mvaddstr(current_y, 0, "[✅] ");
+        window.mvaddstr(current_y, 0, &format!("[{}] ", chars[1]));
         window.attroff(COLOR_PAIR(3));
         current_y+=1;
     }
